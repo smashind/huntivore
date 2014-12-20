c6a43a85e20cc900a25d3fcae56bc12b132359a3
Danny Baumann <danthemanvt@gmail.com>
2014-12-20 06:57:58 -0800

    Remove host from config file

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -14,8 +14,7 @@ CarrierWave.configure do |config|
 		:provider => 'AWS',
 		:aws_access_key_id => ENV['S3_KEY'],
 		:aws_secret_access_key => ENV['S3_SECRET'],
-		:region => 'us-west-2',
-		:host => 'http://huntivore.s3-us-west-2.amazonaws.com/'
+		:region => 'us-west-2'
 	}
 	 
 	# For testing, upload files to local `tmp` folder.
