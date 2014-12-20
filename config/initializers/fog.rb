5fad6faaffda4fc68b9d466e1f83f65ea9d32ac4
Danny Baumann <danthemanvt@gmail.com>
2014-12-20 06:29:38 -0800

    Update fog initializer

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -13,7 +13,8 @@ CarrierWave.configure do |config|
 		# Configuration for Amazon S3
 		:provider => 'AWS',
 		:aws_access_key_id => ENV['S3_KEY'],
-		:aws_secret_access_key => ENV['S3_SECRET']
+		:aws_secret_access_key => ENV['S3_SECRET'],
+		:region => 'us-west-2'
 	}
 	 
 	# For testing, upload files to local `tmp` folder.
