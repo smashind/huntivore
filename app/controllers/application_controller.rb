f668d41186b7c8c7b62f51b60ea5e6718bd737bc
Danny Baumann <danthemanvt@gmail.com>
2015-01-03 06:36:54 -0800

    Another test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  skip_before_filter :verify_authenticity_token
-  # protect_from_forgery with: :exception
+  protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
--- app/controllers/payment_notifications_controller.rb
+++ app/controllers/payment_notifications_controller.rb
@@ -1,5 +1,6 @@
 class PaymentNotificationsController < ApplicationController
 	protect_from_forgery :except => [:create]
+	skip_before_action :verify_authenticity_token
   def create
   	PaymentNotification.create!(params: params, reservation_id: params[:invoice], status: params[:payment_status], transaction_id: params[:txn_id])
   	render nothing: true
