054d0906bd12ba751d8d66a6aeffc029a21762f0
Danny Baumann <danthemanvt@gmail.com>
2015-01-03 05:22:24 -0800

    Updates for Paypal

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
   resources :property_attachments, only: :destroy
 
   post 'payment_notifications', :controller => 'payment_notifications', :action => 'create'
-  match "pages/success", to: 'pages#success', via: [:get, :post]
+  match "pages/success", :to => 'pages#success', via: [:get, :post]
   match 'pages/home' => redirect('/'), via: [:get, :post]
 end
