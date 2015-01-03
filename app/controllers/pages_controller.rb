4ef54ba7d4d6b979b3ae4efe8265848abb624780
Danny Baumann <danthemanvt@gmail.com>
2015-01-03 06:41:35 -0800

    Another test

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
+	skip_before_action :verify_authenticity_token, only: :success
   def home
   	if user_signed_in?
   		redirect_to user_path(current_user)
