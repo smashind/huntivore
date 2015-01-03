53db8c0c97f36c76a260f801462442d1e64e3e4b
Danny Baumann <danthemanvt@gmail.com>
2015-01-03 05:55:59 -0800

    Skip before filter

--- app/controllers/payment_notifications_controller.rb
+++ app/controllers/payment_notifications_controller.rb
@@ -1,5 +1,6 @@
 class PaymentNotificationsController < ApplicationController
 	protect_from_forgery :except => [:create]
+	skip_before_filter :verify_authenticity_token, :only => [:create]
   def create
   	PaymentNotification.create!(params: params, reservation_id: params[:invoice], status: params[:payment_status], transaction_id: params[:txn_id])
   	render nothing: true
