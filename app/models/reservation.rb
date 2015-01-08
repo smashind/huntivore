6976af202992fb15120ca4a38a558f8ac2a1bb7f
Danny Baumann <danthemanvt@gmail.com>
2015-01-08 04:49:54 -0800

    Add support for day hunts

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,14 +3,18 @@ class Reservation < ActiveRecord::Base
   belongs_to :property
   has_many :payment_notifications
   validates :from, presence: true
-  validates :to, presence: true
+  validates :to, presence: true, if: :should_validate_to?
   validates :user_id, presence: true
   validates :property_id, presence: true
 
   validate :date_order
 
   def trip_price
-  	Array(from..to-1).count * property.price
+    if to.nil?
+      property.price
+    else
+  	  Array(from..to-1).count * property.price
+    end
   end
 
   private
@@ -19,5 +23,11 @@ class Reservation < ActiveRecord::Base
     	if self.to && (self.from > self.to)
         self.errors[:base] << "End date must come after start date."
     	end
+    end
+
+    def should_validate_to?
+      if property.hunttype == 'overnight'
+        true
+      end
     end
 end
