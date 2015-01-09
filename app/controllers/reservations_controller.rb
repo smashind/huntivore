c1b36229012d603d73e9d5f31df17b53daf2b14e
Danny Baumann <danthemanvt@gmail.com>
2015-01-09 03:50:46 -0800

    Fix mailer

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -12,12 +12,10 @@ class ReservationsController < ApplicationController
   	@reservation = current_user.reservations.new(reservation_params)
 
   	if @reservation.save
-      if Rails.env.production?
-        user = User.find(params[:reservation][:user_id])
-        property = Property.find(params[:reservation][:property_id])
-        ReservationMailer.reservation_email(user, property, @reservation).deliver
-        ReservationMailer.owner_email(user, property, @reservation).deliver
-      end
+      #if Rails.env.production?
+        ReservationMailer.reservation_email(@reservation.user, @reservation.property, @reservation).deliver
+        ReservationMailer.owner_email(@reservation.user, @reservation.property, @reservation).deliver
+      #end
   		redirect_to @reservation, notice: "Reservation was successfully created."
     else
       redirect_to :back, alert: "Reservation not submitted. Please check your dates and try again." 
