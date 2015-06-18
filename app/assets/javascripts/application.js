93f71b363ef948c9dc24d4564c381f2f242cbfe5
Danny Baumann <danthemanvt@gmail.com>
2015-06-18 15:42:28 -0700

    Add Turbolinks and gem to make it play well with jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,7 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.turbolinks
 //= require bootstrap-sprockets
 //= require jquery_ujs
 //= require jquery-ui
@@ -18,3 +19,4 @@
 //= require jstz
 //= require browser_timezone_rails/set_time_zone
 //= require_tree .
+//= require turbolinks
