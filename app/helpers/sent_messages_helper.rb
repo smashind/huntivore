bece4e9aa88446c74dac4f685bebe8137148ee99
Danny Baumann <danthemanvt@gmail.com>
2015-06-23 04:54:46 -0700

    Add method to calculate unread messages

--- app/helpers/sent_messages_helper.rb
+++ app/helpers/sent_messages_helper.rb
@@ -1,2 +1,25 @@
 module SentMessagesHelper
+	def unread_messages user
+		unread_by_user(user) + unread_by_recipient(user)
+	end
+
+	def unread_by_user user
+		unread_messages = 0
+		user.sent_messages.each do |message| 
+			if !message.read_by_user
+				unread_messages += 1
+			end
+		end
+		unread_messages
+	end
+
+	def unread_by_recipient user
+		unread_total = 0
+		user.messages.each do |message| 
+			if !message.read_by_recipient
+				unread_total += 1
+			end
+		end
+		unread_total
+	end
 end
